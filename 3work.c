869e76483fc5405d6218f84f400abec0ea0ead14
dorou <32395654+dorou@users.noreply.github.com>
2018-03-16 23:29:12 +0300

    Add files via upload
    3

---
+++ 3work.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int result,value1,value2;
+    value1=15;
+    value2=87;
+    result=value2 - value1;
+   printf("The answer is %i\n",result);
+   return 0;
+}
